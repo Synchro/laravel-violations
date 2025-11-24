48b9ec31237b2e5a3d9da771959e52a9b9339090
Marcus Bointon <marcus@synchromedia.co.uk>
2025-11-24 19:15:49 +0100

    Add spatie/csp to suggest

--- src/Support/AddReportingEndpointsPreset.php
+++ src/Support/AddReportingEndpointsPreset.php
@@ -9,6 +9,10 @@ use Spatie\Csp\Policy;
 use Spatie\Csp\Preset;
 use Synchro\Violation\Violation;
 
+/**
+ * A preset that adds reporting endpoints for CSP violation reports using Spatie's CSP package,
+ * So add that dependency if you want to make use of this preset.
+ */
 class AddReportingEndpointsPreset implements Preset
 {
     public function configure(Policy $policy): void
